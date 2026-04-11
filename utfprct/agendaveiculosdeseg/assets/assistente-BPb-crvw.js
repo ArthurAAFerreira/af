b16dfcc66f78dd323173f604bd4b93c9086b7e93
Arthur Ferreira <arthuraaferreira@gmail.com>
2026-04-11 10:41:12 -0300

    Rebuild CT+LD: fix asset references after manual HTML edits

---
+++ utfprct/agendaveiculosdeseg/assets/assistente-BPb-crvw.js
@@ -0,0 +1,1 @@
+import{v as t}from"./nav-BZFhFHG5-BZFhFHG5-BZFhFHG5-BZFhFHG5-BZFhFHG5-BZFhFHG5-BZFhFHG5-DJM8iS2l.js";t("assistente.html");
---
+++ utfprld/agendaveiculosdeseg/assets/assistente-CJWBPndR.js
@@ -0,0 +1,1 @@
+import{v as t}from"./nav-BZFhFHG5-BZFhFHG5-BZFhFHG5-BZFhFHG5-BZFhFHG5-BZFhFHG5-BZFhFHG5-pJ6HFEH_.js";t("assistente.html");
