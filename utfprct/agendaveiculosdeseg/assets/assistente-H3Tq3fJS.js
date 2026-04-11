7cd67e6a2e4981d47afd35e7244ff26ebba41d17
Arthur Ferreira <arthuraaferreira@gmail.com>
2026-04-11 10:33:05 -0300

    Fix: label Matricula/CPF -> Matricula in motorista modal (CT+LD)

---
+++ utfprct/agendaveiculosdeseg/assets/assistente-H3Tq3fJS.js
@@ -0,0 +1,1 @@
+import{v as t}from"./nav-BZFhFHG5-BZFhFHG5-BZFhFHG5-BZFhFHG5-BZFhFHG5-BZFhFHG5-Ov7UyS1W.js";t("assistente.html");
---
+++ utfprld/agendaveiculosdeseg/assets/assistente-bL3GIKip.js
@@ -0,0 +1,1 @@
+import{v as t}from"./nav-BZFhFHG5-BZFhFHG5-BZFhFHG5-BZFhFHG5-BZFhFHG5-BZFhFHG5-C24jrjzs.js";t("assistente.html");
